ac524db534b4eb92ac8902b62b74880f379c1d71
kbenmus02 <mkbensmaia@gmail.com>
2022-01-18 10:28:22 -0500

    fix adress localhost

--- run_app.py
+++ run_app.py
@@ -2,5 +2,5 @@ from app import app
 import os
 
 if __name__ == '__main__':
-    port = os.environ.get("PORT", 5000)
-    app.run(debug=False, host="0.0.0.0", port = port)
+    #port = os.environ.get("PORT", 5000)
+    app.run(debug=False)#, host="0.0.0.0", port = port)
